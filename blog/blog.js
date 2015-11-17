6c14563d9a38ceef65e25a03643b34ac706776bb
ryuqing <operantioncat@.com>
2015-11-17 08:14:27 +0800

    blog

--- blog/blog.js
+++ blog/blog.js
@@ -2,8 +2,8 @@ $(function(){
 	/*只要给li标签添加 fstmenuclass 就可以做无限级*/
 	var menu = $(".blog-header-menu .fstmenu");
 	menu.hover(function(){
-		$(this).children("ul").show("200");//slow 让菜单徐徐向下 也可以用.fadeIn()（渐入渐出）
+		$(this).children("ul").stop(true,true).slideDown('slow');//slow 让菜单徐徐向下 也可以用.fadeIn()（渐入渐出）
 	},function(){
-		$(this).children("ul").hide();
+		$(this).children("ul").stop(true,true).slideUp('slow');
 	});
 });
