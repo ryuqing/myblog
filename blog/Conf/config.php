d08395059e1b789febdf86b4528e539c9ba2b76a
ryuqing <raoyuqing@aragoncs.com>
2015-11-19 16:54:18 +0800

    valid

--- blog/Conf/config.php
+++ blog/Conf/config.php
@@ -1,5 +1,7 @@
 <?php
 return array(
 	//'配置项'=>'配置值'
+	'DB_PREFIX'=>'wp_',  //设置表前缀
+	'DB_DSN'=>'mysql://root:@localhost:3306/myblog',
 );
 ?>
--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php 
 /*前台入口文件*/
-
+define('APP_DEBUG',true);
 //1.应用名称 Home  所有目录名称首字母都要大写
 define('APP_NAME', 'Blog');//这边创建前台应用文件夹
 //2.确定应用路径 /很重要
