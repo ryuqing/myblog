601398f79e2c589b0d0a33d81fc8855ac52741b7
ryuqing <raoyuqing@aragoncs.com>
2015-11-19 17:10:50 +0800

    office

--- blog/Conf/config.php
+++ blog/Conf/config.php
@@ -1,7 +1,9 @@
 <?php
 return array(
 	//'配置项'=>'配置值'
-	'DB_PREFIX'=>'wp_',  //设置表前缀
-	'DB_DSN'=>'mysql://root:@localhost:3306/myblog',
+	'TMPL_L_DELIM'=>'<{', //修改左定界符
+	'TMPL_R_DELIM'=>'}>', //修改右定界符
+	'DB_DSN'=>'mysql://root:@localhost:3306/myblog',//DSN方法 
+	'SHOW_PAGE_TRACE'=>true,//开启页面Trace 跟踪	
 );
 ?>
--- blog/Lib/Action/IndexAction.class.php
+++ blog/Lib/Action/IndexAction.class.php
@@ -2,8 +2,5 @@
 // 本类由系统自动生成，仅供测试用途
 class IndexAction extends Action {
     public function index(){
-    	$m =new Model('Users');
-    	$arr = $m->select();
-    	print_r($arr);
     }
 }
