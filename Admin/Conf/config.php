457e86417e17c709d1d84ff44a0dadb204b15b53
ryuqing <operantioncat@.com>
2015-12-20 11:24:23 +0800

    home

--- Admin/Conf/config.php
+++ Admin/Conf/config.php
@@ -2,8 +2,8 @@
 return array(
 	//'配置项'=>'配置值'
 	'APP_DEBUG'=>TRUE, 
-	'TMPL_L_DELIM' => '<{', //修改左定界符<{
-	'TMPL_R_DELIM' => '}>', //修改右定界符}>
+	'TMPL_L_DELIM'=>'<{', //修改左定界符
+	'TMPL_R_DELIM'=>'}>', //修改右定界符
 	'DB_TYPE' => 'mysql',
 	'DB_HOST' => 'localhost',
 	'DB_USER' => 'root',
