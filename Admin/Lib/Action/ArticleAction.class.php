e25c8a0c542bcfe150cc904fbfd47c79c7c53daa
qq <raoyuqing@aragoncs.com>
2015-12-31 11:05:33 +0800

    dh

--- Admin/Lib/Action/ArticleAction.class.php
+++ Admin/Lib/Action/ArticleAction.class.php
@@ -3,14 +3,16 @@ class ArticleAction extends Action{
 	public function index(){
 		$this->display();
 	}
-
+	public function postnew(){
+		$this->display();
+	}
 	public function postAdd(){
 		$data['post_title'] = $_POST['post_title'];
-		$data['post_content'] = $_POST['editorValue'];
 		$data['post_parent'] = $_POST['post_parent'];
-
+		$data['post_content'] = $_POST['editorValue'];
+		print_r($data);
 	}
-}
 
+}
 
 ?>
