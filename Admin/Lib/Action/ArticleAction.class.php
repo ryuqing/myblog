2e11360c95c23afccce7796d3d90d8229d34f5d3
qq <raoyuqing@aragoncs.com>
2015-12-31 11:09:38 +0800

    request

--- Admin/Lib/Action/ArticleAction.class.php
+++ Admin/Lib/Action/ArticleAction.class.php
@@ -10,7 +10,6 @@ class ArticleAction extends Action{
 		$data['post_title'] = $_POST['post_title'];
 		$data['post_parent'] = $_POST['post_parent'];
 		$data['post_content'] = $_POST['editorValue'];
-		print_r($data);	
 	}
 
 }
